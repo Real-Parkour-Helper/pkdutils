ed56d7a45c1919e770c66bf0fce01766b87f3882
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2025-04-05 17:12:11 +0200

    feat: track loaded chunks and provide easy access using world coordinates

--- src/proxy/modules/World.ts
+++ src/proxy/modules/World.ts
@@ -1,6 +1,8 @@
 import { PacketInterceptor } from "../packet/PacketInterceptor"
 import { Packet } from "../packet/Packet"
-import * as fs from "node:fs"
+import { Vec3 } from "vec3"
+
+const Chunk = require("prismarine-chunk")("1.8.9")
 
 /**
  * World class for tracking chunk data and storing an
@@ -11,9 +13,46 @@ export class World extends PacketInterceptor {
     super("World", "1.0.0")
   }
 
+  private static chunks: Map<string, typeof Chunk> = new Map()
+
+  /**
+   * Get a block at a given position. This will
+   * return null if the block is in a chunk
+   * that hasn't been received by the client yet.
+   * @param x
+   * @param y
+   * @param z
+   */
+  static getBlock(x: number, y: number, z: number) {
+    const chunkX = x >> 4
+    const chunkZ = z >> 4
+    const chunkKey = `${chunkX},${chunkZ}`
+    const chunk = World.chunks.get(chunkKey)
+
+    if (!chunk) return null
+    const blockX = x & 0xF
+    const blockZ = z & 0xF
+    const blockY = y & 0xFF // technically not needed, but it's good to make sure it's in a valid range anyway...
+
+    return chunk.getBlock(new Vec3(blockX, blockY, blockZ))
+  }
+
   incomingPacket(packet: Packet): Packet {
+    if (packet.meta.name === "respawn") {
+      // On switching dimensions / worlds, clear chunks
+      World.chunks.clear()
+    }
+
     if (packet.meta.name === "map_chunk") {
       // Handle chunks
+      const chunkKey = `${packet.data.x},${packet.data.z}`
+
+      // I'm not sure what to do for this, or what the server is trying to achieve here. Maybe a whole chunk update?
+      if (World.chunks.has(chunkKey)) return packet
+
+      let chunk = new Chunk()
+      chunk.load(packet.data.chunkData, packet.data.bitMap ?? 0xFFFF)
+      World.chunks.set(chunkKey, chunk)
     }
 
     return packet
