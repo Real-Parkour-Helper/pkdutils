05059006d8d862c27985cb8956c0936a510ed05f
senez333 <airh4ck@yandex.ru>
2025-04-15 20:35:53 +0300

    fix: remove `/parkour` command because it interferes with hypixel's command

--- src/proxy/commands/ParkourCommand.ts
+++ src/proxy/commands/ParkourCommand.ts
@@ -3,7 +3,7 @@ import { Packet } from "../packet/Packet"
 
 export class ParkourCommand extends Command {
   constructor() {
-    super("ParkourCommand", "1.0.0", ["parkour", "pkd"], [])
+    super("ParkourCommand", "1.0.0", ["pkd"], [])
   }
 
   protected handle(args: string[], packet: Packet) {
