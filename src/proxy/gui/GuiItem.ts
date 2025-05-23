727a97a060fbe64f09580923d77208fa918792b6
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2025-05-23 18:28:33 +0200

    gui item class

---
+++ src/proxy/gui/GuiItem.ts
@@ -0,0 +1,40 @@
+export class GuiItem {
+
+  /**
+   * GuiItem constructor
+   * @param id
+   * @param name
+   * @param count
+   * @param damage
+   */
+  constructor(public id: number, public name: string, public count: number, public damage: number | undefined) {}
+
+
+  /**
+   * Convert the GuiItem to a JSON string
+   * to be sent in a packet
+   */
+  toJSON(): string {
+    return JSON.stringify({
+      blockId: this.id,
+      itemCount: this.count,
+      itemDamage: this.damage,
+      nbtData: {
+        "type": "compound",
+        "name": "",
+        "value": {
+          "display": {
+            "type": "compound",
+            "value": {
+              "Name": {
+                "type": "string",
+                "value": this.name
+              }
+            }
+          }
+        }
+      }
+    })
+  }
+
+}
